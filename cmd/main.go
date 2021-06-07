171e81b3a3b868ddea8a7c73c442efbbc81caf48
grvr <goritss@gmail.com>
2021-06-07 13:10:35 +0500

    Settings for DB

--- cmd/main.go
+++ cmd/main.go
@@ -1,19 +1,37 @@
 package main
 
 import (
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
 	"github.com/satanCoffee/todo-app-golang"
 	"github.com/satanCoffee/todo-app-golang/pkg/handler"
 	"github.com/satanCoffee/todo-app-golang/pkg/repository"
 	"github.com/satanCoffee/todo-app-golang/pkg/service"
 	"github.com/spf13/viper"
 	"log"
+	"os"
 )
 
-func main()  {
+func main() {
 	if err := initConfig(); err != nil {
 		log.Fatalf("error initializing configs: %s", err.Error())
 	}
-	repos := repository.NewRepository()
+
+	if err := godotenv.Load(); err != nil {
+		log.Fatalf("error loading env variables: %s", err.Error())
+	}
+	db, err := repository.NewPostgresDB(repository.Config{
+		Host:     viper.GetString("db.host"),
+		Port:     viper.GetString("db.port"),
+		Username: viper.GetString("db.username"),
+		DBName:   viper.GetString("db.dbname"),
+		SSLMode:  viper.GetString("db.sslmode"),
+		Password: os.Getenv("DB_PASSWORD"),
+	})
+	if err != nil {
+		log.Fatalf("erro initializing configs: %s", err.Error())
+	}
+	repos := repository.NewRepository(db)
 	services := service.NewService(repos)
 	handlers := handler.NewHandler(services)
 
--- schema/000001_init.up.sql
+++ schema/000001_init.up.sql
@@ -25,7 +25,7 @@ CREATE TABLE todo_items
   id serial not null unique,
   title varchar(255) not null,
   description varchar(255),
-  done boolean not null default false,
+  done boolean not null default false
 );
 
 CREATE TABLE lists_items
